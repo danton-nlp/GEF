911d38bd36728ccdd99790882ef1c7176bb61918
Daniel Levenson <dleve123@gmail.com>
2022-05-14 17:43:39 -0400

    refactor: remove causal handling code
    - likely need a different model, so doesn't really make sure to keep it
      around...

--- tests/test_compute_probs.py
+++ tests/test_compute_probs.py
@@ -4,7 +4,6 @@ import pytest
 from compute_probs import (
     build_masked_inputs_and_targets,
     compute_prior_and_posterior_probs,
-    compute_prior_probs,
 )
 from src.generation_utils import load_model_and_tokenizer, load_xsum_with_mask_in_vocab
 
@@ -39,33 +38,6 @@ def long_entity_data():
             ],
         }
     )
-
-
-# @pytest.fixture(scope="module")
-# def causal_multiple_entity_data():
-#     return build_masked_inputs_and_targets(
-#         {
-#             "source": 'The 58-year-old spent three months in charge of the Addicks at the end of the 2013-14 campaign, keeping the club in the Championship. Since leaving The Valley the Belgian has spent time in charge of Blackpool, Standard Liege and Metz. Riga replaces compatriot Karel Fraeye, who was sacked from his post as interim head coach on Wednesday. Charlton are currently 23rd in the Championship table, three points from safety, and are on a run of 10 games without a win in all competitions. Fraeye was appointed in late October following the departure of Guy Luzon, but only won two of his 14 matches in charge of the first team. In a statement on the club website, Addicks owner Roland Duchatelet admitted the club had made errors in player recruitment and said the board of directors accepted responsibility for "a disappointing season". "It was crucial we dealt with the position of the head coach," the Belgian businessman added. "Jose did an excellent job in his short period with Charlton two seasons ago. He was very popular with supporters and I believe that he will get us back on track." Riga won seven of his 16 games during his stint at The Valley in 2014 but left the south-east London club when his contract was not renewed that summer and joined Blackpool. BBC Radio London\'s Andy Rowley. Charlton fans are increasingly angry with how the club is being run by Roland Duchatelet, who is now onto his sixth head coach since taking over the club in January 2014. There have been a number of recent protests at The Valley aimed at Duchatelet and chief executive Katrien Meire from supporters, who have now come together to form a group called "Coalition Against Roland Duchatelet" in an attempt to bring about a sale of the club. Riga has far more managerial experience than his predecessor Karel Fraeye but, given his previous links to Duchatelet and the antipathy towards the board of directors, the appointment could only serve to fan the flames for further supporter unrest.',
-#             "reference": "Championship strugglers Charlton Athletic have reappointed Jose Riga as head coach on an 18-month deal.",
-#             "prediction": "Charlton Athletic have appointed Jose Riga as their new head coach on a two-year contract.",
-#             "entities": [
-#                 {
-#                     "start": 33,
-#                     "end": 42,
-#                     "label": "Non-hallucinated",
-#                     "type": "GPE",
-#                     "ent": "Jose Riga",
-#                 },
-#                 {
-#                     "start": 72,
-#                     "end": 80,
-#                     "label": "Non-factual Hallucination",
-#                     "type": "DATE",
-#                     "ent": "two-year",
-#                 },
-#             ],
-#         }
-#     )
 
 
 @pytest.fixture(scope="module")
@@ -141,19 +113,6 @@ def evaluation_repro_data():
             ],
         }
     )
-
-
-@pytest.mark.skip("not targeting causal modeling for now")
-def test_causal_many_entities(bart_large, causal_multiple_entity_data):
-    inputs, targets, entities, sources, _labels = causal_multiple_entity_data
-
-    ne_probs = compute_prior_probs(inputs, targets, entities, bart_large, verbose=True)
-    print(ne_probs)
-
-    assert ne_probs == [
-        (0.000604625849518925 * 0.014603929594159126 * 0.14980721473693848),
-        (0.4591749906539917 * 0.9699639678001404 * 0.9490254521369934),
-    ]
 
 
 # masked_input='Charlton Athletic have appointed <mask> as their new head coach on a two-year contract.'
