e7f59aa7d0ddbe2cb676fcd17628d76ddf3f46b7
Anton Abilov <anton.abilov@gmail.com>
2022-06-03 10:10:51 -0400

    Fixed #37, improved PERSON entity splitting (consistent with X-Ent)
    - Don't split on special char start/ending " ' ` - _
    - Don't split when it starts with "St"
    
    Added tests cases & updated annotations
    
    Updated batch detected entities after fixing name splitting

--- tests/test_detect_entities.py
+++ tests/test_detect_entities.py
@@ -35,3 +35,67 @@ def test_split_person():
     assert entities[1]["type"] == "PERSON"
     assert not entities[1]["in_source"]
     assert summary[entities[1]["start"] : entities[1]["end"]] == entities[1]["ent"]
+
+
+def test_split_person_apostrophe():
+    summary = "A search is under way for Daniel Levenson's cat"
+    source = "Pembrokeshire"
+    entities = detect_entities(summary, source)
+
+    assert len(entities) == 2
+
+    assert entities[0]["ent"] == "Daniel"
+    assert entities[0]["type"] == "PERSON"
+    assert not entities[0]["in_source"]
+    assert summary[entities[0]["start"] : entities[0]["end"]] == entities[0]["ent"]
+
+    assert entities[1]["ent"] == "Levenson's"
+    assert entities[1]["type"] == "PERSON"
+    assert not entities[1]["in_source"]
+    assert summary[entities[1]["start"] : entities[1]["end"]] == entities[1]["ent"]
+
+
+def test_split_person_quote():
+    summary = (
+        'John "Brad" Bradbury, drummer with The Specials, has died at the age of 62.'
+    )
+    source = "Pembrokeshire"
+    entities = detect_entities(summary, source)
+
+    assert len(entities) == 5
+
+    assert entities[0]["ent"] == "John"
+    assert entities[0]["type"] == "PERSON"
+    assert not entities[0]["in_source"]
+    assert summary[entities[0]["start"] : entities[0]["end"]] == entities[0]["ent"]
+
+    assert entities[1]["ent"] == '"Brad"'
+    assert entities[1]["type"] == "PERSON"
+    assert not entities[1]["in_source"]
+    assert summary[entities[1]["start"] : entities[1]["end"]] == entities[1]["ent"]
+
+
+def test_split_person_dash():
+    summary = "A search is under way for Yong-Han"
+    source = "Pembrokeshire"
+    entities = detect_entities(summary, source)
+
+    assert len(entities) == 1
+
+    assert entities[0]["ent"] == "Yong-Han"
+    assert entities[0]["type"] == "PERSON"
+    assert not entities[0]["in_source"]
+    assert summary[entities[0]["start"] : entities[0]["end"]] == entities[0]["ent"]
+
+
+def test_split_person_st():
+    summary = "A search is under way for the person St Johnstone"
+    source = "Pembrokeshire"
+    entities = detect_entities(summary, source)
+
+    assert len(entities) == 1
+
+    assert entities[0]["ent"] == "St Johnstone"
+    assert entities[0]["type"] == "PERSON"
+    assert not entities[0]["in_source"]
+    assert summary[entities[0]["start"] : entities[0]["end"]] == entities[0]["ent"]
