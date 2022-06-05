3bb5b25a3d946b83f96d1b3b91e3cb25f9a9ca77
Anton Abilov <anton.abilov@gmail.com>
2022-06-05 09:15:45 -0400

    Revert "Fixed #37, improved PERSON entity splitting (consistent with X-Ent)"
    This reverts commit e7f59aa7d0ddbe2cb676fcd17628d76ddf3f46b7.

--- src/detect_entities.py
+++ src/detect_entities.py
@@ -1,27 +1,9 @@
-from typing import List
+from typing import List, TypedDict
 import spacy
-from spacy.tokens import Span
 from src.entity_utils import MarkedEntity, is_entity_contained
 
+
 nlp = spacy.load("en_core_web_lg")
-
-
-def split_person_entity(entity: Span, source: str) -> List[MarkedEntity]:
-    split_entity: List[MarkedEntity] = []
-    ent_parts = entity.text.split(" ")
-    start_idx = entity[0].idx
-    for sub_ent in ent_parts:
-        split_entity.append(
-            {
-                "ent": sub_ent,
-                "type": "PERSON",
-                "start": start_idx,
-                "end": start_idx + len(sub_ent),
-                "in_source": is_entity_contained(sub_ent, source),
-            }
-        )
-        start_idx += len(sub_ent) + 1
-    return split_entity
 
 
 def detect_entities(summary: str, source: str) -> List[MarkedEntity]:
@@ -30,12 +12,22 @@ def detect_entities(summary: str, source: str) -> List[MarkedEntity]:
     marked_entities: List[MarkedEntity] = []
     for entity in nlp_summary.ents:
         # split person entities
-        if (
-            entity[0].ent_type_ == "PERSON"
-            and len(entity) > 1
-            and entity[0].text != "St"
-        ):
-            marked_entities += split_person_entity(entity, source)
+        # hard-coding "St" case because it's not split in entfa
+        if entity[0].ent_type_ == "PERSON" and len(entity) > 1 and entity[0].text != "St":
+            for entity_token in entity:
+                # NB: this is handled slightly different 
+                # from EntFA, they include 's in the last token
+                # but there's only a few cases so I presume we're good
+                if entity_token.text != "'s":
+                    marked_entities.append(
+                        {
+                            "ent": entity_token.text,
+                            "type": entity_token.ent_type_,
+                            "start": entity_token.idx,
+                            "end": entity_token.idx + len(entity_token.text),
+                            "in_source": is_entity_contained(entity_token.text, source),
+                        }
+                    )
         else:
             marked_entities.append(
                 {
