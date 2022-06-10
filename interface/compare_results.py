66b76c64cd6b0d2a5e8eff63682ab0ebc0ca2285
Anton Abilov <anton.abilov@gmail.com>
2022-06-10 16:24:16 -0400

    Updated interface for comparing results (selection of model/columns)

--- interface/compare_results.py
+++ interface/compare_results.py
@@ -8,10 +8,7 @@ MODELS = []
 def load_summaries_df(path: str):
     with open(path, "r") as f:
         json_sums = json.load(f)
-        df = pd.DataFrame(
-            json_sums.values(),
-            index=list(json_sums.keys())
-        )
+        df = pd.DataFrame(json_sums.values(), index=list(json_sums.keys()))
         return df
 
 
@@ -37,8 +34,31 @@ def render_compare_results():
     df_sums = load_summaries_df(f"{filename}-summaries.json")
     st.title("Compare Results")
 
+    selected_models = st.multiselect(
+        "Select models",
+        options=df_aggregated.index,
+        default=[
+            "fbs_classifier",
+            "fbs_oracle"
+        ],
+    )
+    selected_columns = st.multiselect(
+        "Select columns",
+        options=df_aggregated.columns,
+        default=[
+            "factual",
+            "non_factual",
+            "non_factual_intrinsic",
+            "non_factual_extrinsic",
+            "skipped",
+            "rouge1",
+            "rouge2",
+            "rougeL",
+        ],
+    )
+
     st.subheader("Human annotation")
-    st.dataframe(df_aggregated)
+    st.dataframe(df_aggregated[selected_columns].loc[selected_models])
     st.subheader("Compare models")
     col1, col2 = st.columns(2)
     non_factual_model = col1.selectbox(
